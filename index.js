c80806bab4fa0ec63c118c1f1354d08095e734d6
josdic1 <joshuadicker@gmail.com>
2025-01-10 13:12:43 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Code your solution here
+const drivers = [
+   {
+     name: "Bobby",
+     hometown: "Pittsburgh",
+   },
+   {
+     name: "Sammy",
+     hometown: "New York",
+   },
+   {
+     name: "Sally",
+     hometown: "Cleveland",
+   },
+   {
+     name: "Annette",
+     hometown: "Los Angeles",
+   },
+   {
+     name: "Bobby",
+     hometown: "Tampa Bay",
+   },
+ ];
+
+ const nameList = drivers.map((driver) => driver.name);
+ console.log(nameList); 
+ 
+ function findMatching(list, name) {
+  if(!name || typeof name !== 'string') {
+    return []
+  } else {
+    const casedName = name.toLowerCase()
+    const matchList = list.filter(driver => driver.toLowerCase() === casedName)
+   return matchList
+  }
+
+ }
+
+ function fuzzyMatch(list, input) {
+  const inputCased = !input ? console.error('EMPTY INPUT') : input.toLowerCase()
+  const matchList = list.filter(item => item.toLowerCase().startsWith(inputCased))
+ return matchList
+ }
+
+ function matchName(list, name) {
+  const casedName = !name ? '' : name.toLowerCase()
+  const matchList = list.filter(item => item.name.toLowerCase() === casedName)
+  return matchList
+}
+ 
+ console.log('fuzzyMatch: ', fuzzyMatch(nameList, ""))
+ console.log('matchName: ', matchName(drivers, "bobby"))
